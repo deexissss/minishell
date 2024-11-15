bc22c7f986c083ebc90608b22a23b7c73b22c650
deexissss <104828869+deexissss@users.noreply.github.com>
2024-11-15 12:40:18 +0000

    addfunct

--- minishell.c
+++ minishell.c
@@ -45,125 +45,6 @@ int main()
         return 0;
 }*/
 
-void    syntax_error(char *inpt)
-{
-    if (memcmp(inpt, ">", 2) == 0)
-        ft_printf("syntax error");
-    if (memcmp(inpt, "<", 2) == 0)
-        ft_printf("syntax error");
-    if (memcmp(inpt, ">>", 2) == 0)
-        ft_printf("syntax error");
-    if (memcmp(inpt, "<<", 2) == 0)
-        ft_printf("syntax error");
-    if (memcmp(inpt, "<>", 2) == 0)
-        ft_printf("syntax error");
-}
-  
-void    execute_lsl()
-{
-    char *args[] = {"/bin/ls", "-l", NULL};
-    pid_t pid = fork();
-
-    if (pid == -1)
-        perror("fork");
-    else if (pid == 0)
-    {
-        if (execve(args[0], args, NULL) == -1)
-        {
-            perror("execve");
-            _exit(1);
-        }
-    }
-    else
-        waitpid(pid, NULL, 0);
-}
-
-void    execute_ls()
-{
-    char *args[] = {"/bin/ls", NULL};
-    pid_t pid = fork();
-
-    if (pid == -1)
-        perror("fork");
-    else if (pid == 0)
-    {
-        if (execve(args[0], args, NULL) == -1)
-        {
-            perror("execve");
-            _exit(1);
-        }
-    }
-    else
-        waitpid(pid, NULL, 0);
-}
-
-void    execute_pwd()
-{
-    char *args[] = {"/bin/pwd", NULL};
-    pid_t pid = fork();
-
-    if (pid == -1)
-        perror("fork");
-    else if (pid == 0)
-    {
-        if (execve(args[0], args, NULL) == -1)
-        {
-            perror("execve");
-            _exit(1);
-        }
-    }
-    else 
-        waitpid(pid, NULL, 0);
-}
-
-void    execute_echo(char *inpt)
-{
-    int     i;
-
-    i = 4;
-    while (inpt[i] == ' ' || inpt[i] == '\t')
-        i++;
-    while (inpt[i])
-    {
-        write(1, &inpt[i], 1);
-        i++;
-    }
-}
-
-void    execute_sleep(char *inpt)
-{
-    int     i;
-    int     time;
-    pid_t pid = fork();
-
-    i = 5;
-    time = 0;
-    while (inpt[i] == ' ' || inpt[i] == '\t')
-        i++;
-    if (inpt[i] == '\0')
-        ft_printf("error : sleep need a time");
-    while (inpt[i] >= '0' && inpt[i] <= '9')
-    {
-        time = time * 10 + (inpt[i] - '0');
-        i++;
-    }
-    if (pid == -1)
-        perror("fork");
-    else if (pid == 0)
-    {
-        char time_str[20];
-        snprintf(time_str, sizeof(time_str), "%d", time);
-        char *args[] = {"sleep", time_str, NULL};
-        if (execvp(args[0], args) == -1)
-        {
-            perror("execvp");
-            _exit(1);
-        }
-    }
-    else
-        waitpid(pid, NULL, 0);
-}
-
 
 void ft_checker(char *inpt)
 {
@@ -179,6 +60,8 @@ void ft_checker(char *inpt)
         execute_pwd();
     if (memcmp(inpt, "sleep", 5) == 0)
         execute_sleep(inpt);
+    if (memcmp(inpt, "cat", 3) == 0)
+        execute_cat(inpt);
 }
 
 int main()
@@ -198,6 +81,7 @@ int main()
     }
     return 0;
 }
+
 /*
 void	ft_initialize(t_data *data)
 {
