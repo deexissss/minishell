f7b2c4ca5269a9677cef5e8a3c94fd1651a48d7a
ryongbox <ryonbox@hotmail.com>
2024-12-08 20:43:10 +0000

    added pipe specific tokenizer, strncpy

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjehaes <tjehaes@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/19 12:30:12 by tjehaes           #+#    #+#             */
-/*   Updated: 2024/02/26 17:18:06 by tjehaes          ###   ########.fr       */
+/*   Created: 2024/02/20 11:06:04 by tjehaes           #+#    #+#             */
+/*   Updated: 2024/02/26 17:18:27 by tjehaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
-	return (i);
+	}
+	return (dest);
 }
